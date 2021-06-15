20eaf9a60dcd9c7ad4d30090ddf77df69a95c7e6
igor2533 <igor25_33@mail.ru>
2021-06-15 22:10:45 +0300

    create default model Products

---
+++ basic/models/Products.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "products".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string|null $price
+ */
+class Products extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'products';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title'], 'required'],
+            [['price'], 'string'],
+            [['title'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'price' => 'Price',
+        ];
+    }
+}
